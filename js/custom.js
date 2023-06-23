183a6ec5e51a3f2e395644cd239fa99b5e49801b
Vishal_Umaraniya <125950260+VishalUmarnaiya@users.noreply.github.com>
2023-06-23 12:14:42 +0530

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+// $('.card')
+
+// gsap.to(".card",{
+//     ease: "linear",
+//     scrollTrigger:{
+//         startTrigger: '.cube-container',
+//         start: "top center",
+//         // endTrigger: ".cube .card:last-child",
+//         // end: "bottom center",
+//         // pin: true,
+//         // pinSpacing: true,
+//         markers: true,
+//         toggleClass: "active"
+//     }
+// })
+
+// // gsap.to(".cube",{
+// //     ease: "linear",
+// //     rotateY:"360deg", 
+// //     y: "-3360px",
+// //     scrollTrigger:{
+// //         trigger:".cube",
+// //         start: "top center",
+// //         endTrigger: ".cube .card:last-child",
+// //         end: "bottom center",
+// //         markers: true,
+// //         scrub: 2,
+// //     }
+// // })
+// $(document).ready(function () {
+//     let i = 0;
+//     $(window).scroll(function () {
+
+//         let cardList = $('.card')
+//         setTimeout(function () {
+//             $('.cube').addClass('active')
+//             $(cardList[i]).addClass('active')
+//             i = i+1;
+//             // console.log(++i)
+
+//         }, 1000)
+//     })
+// })
